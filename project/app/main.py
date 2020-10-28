68b96c5f650bf13db3c2bd86eec0ba38930ff426
Anton Dorozhko <dorozhko.a@gmail.com>
2020-10-28 04:04:37 +0100

    docker-compose example

--- project/app/main.py
+++ project/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Depends
 from app.config import get_settings, Settings
-
+import redis
 
 app = FastAPI()
 
@@ -21,5 +21,15 @@ def pong2(settings: Settings = Depends(get_settings)):
     return {
         "ping": "pong!",
         "environment": settings.environment,
+    }
+
+
+@app.get("/redis")
+def redis_check():
+    r = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
+    r.set('foo', 'bar')
+    return {
+        'redis_foo' : r.get('foo'),
+        'OK' : r.get('foo') == "bar"
     }
 
